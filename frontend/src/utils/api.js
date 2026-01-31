983372482a54928ddf689b62031259e3db01ee63
Nileshkantamani <nileshkantamani@gmail.com>
2026-01-31 16:38:56 +0530

    Version 4

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,47 +1,25 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('auth_token')
-    
-    console.log('API Request:', {
-      url: config.url,
-      hasToken: !!token,
-      tokenPreview: token ? token.substring(0, 20) + '...' : 'none'
-    })
-    
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
-    
+
     return config
   },
-  (error) => {
-    console.error('Request interceptor error:', error)
-    return Promise.reject(error)
-  }
+  (error) => Promise.reject(error)
 )
 
 api.interceptors.response.use(
-  (response) => {
-    console.log('API Response:', {
-      url: response.config.url,
-      status: response.status
-    })
-    return response
-  },
-  (error) => {
-    console.error('API Error:', {
-      url: error.config?.url,
-      status: error.response?.status,
-      detail: error.response?.data?.detail
-    })
-    return Promise.reject(error)
-  }
+  (response) => response,
+  (error) => Promise.reject(error)
 )
 
 export default api
